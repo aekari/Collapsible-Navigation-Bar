0f90da34913f57b1bbe3a90011d7a37afc690e56
ACE User <aceuser@e140-s22.lab.lagcc>
2017-06-12 21:13:14 -0400

    created functional navbar toggle in JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('Woohoo!');
+var topNavList = document.querySelector('#nav-list');
+var topNavToggle = document.querySelector('#nav-toggle');
+var navCollapsed = true;
+
+topNavToggle.addEventListener('click', function(){
+	if(navCollapsed === true){
+		topNavList.classList.remove('navbar-collapsed');
+		navCollapsed = false;
+		topNavToggle.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
+	} else {
+		topNavList.classList.add('navbar-collapsed');
+		navCollapsed = true;
+		topNavToggle.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>';
+	}
+});
