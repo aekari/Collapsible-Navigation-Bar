bbac8416af2285702918971b4ee5cb031cf44c30
aekari <ashleighekari@gmail.com>
2023-02-28 13:25:02 -0800

    updated README and added commentary to script.js file

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 This is a portfolio that links to various projects I have created.
 
 ## Features
--A collapsable navbar
+-A collapsible navbar
+![](https://i.imgur.com/LZwQOma.gif)
--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 var topNavList = document.querySelector('#nav-list');
 var topNavToggle = document.querySelector('#nav-toggle');
-var navCollapsed = true;
+var navCollapsed = true;	// by default, the nav list is in a collapsed state
 
-topNavToggle.addEventListener('click', function(){
-	if(navCollapsed === true){
-		topNavList.classList.remove('navbar-collapsed');
-		navCollapsed = false;
-		topNavToggle.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
-	} else {
-		topNavList.classList.add('navbar-collapsed');
-		navCollapsed = true;
-		topNavToggle.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>';
+// the following code opens and closes the topNav when you click on it
+
+topNavToggle.addEventListener('click', function(){ // the nav will have an event listener and do the following:
+	if(navCollapsed === true){	// if navCollapsed is strictly true, do the following:
+		topNavList.classList.remove('navbar-collapsed'); // remove the ul list element (aka, topNavList)
+		navCollapsed = false; // set navbarCollapsed to false, and
+		topNavToggle.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>'; // hide the Font Awesome x icon (fa-times)
+	} else { // otherwise
+		topNavList.classList.add('navbar-collapsed'); // add the ul list element, and
+		navCollapsed = true; // set the navCollapsed value to the boolean value, true, and 
+		topNavToggle.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>'; // hide the  Font Awesome hamburger icon (fa-bars)
 	}
 });
